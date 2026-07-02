386735f93d67871cd1c2b22bf76483e543c9c928
Anandu <ananduep93@gmail.com>
2026-07-02 21:57:21 +0530

    Commit config.js and update .gitignore to enable active Firebase database and ImageBB uploads on Vercel

---
+++ assets/js/config.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAik95yxNNzDVF8Xc5zK7Vf_-mZ-EpPfd0",
+  authDomain: "student-id-a8fa4.firebaseapp.com",
+  projectId: "student-id-a8fa4",
+  storageBucket: "student-id-a8fa4.firebasestorage.app",
+  messagingSenderId: "984650177598",
+  appId: "1:984650177598:web:c551e6f8fe0fb43c24856f",
+  measurementId: "G-LHDHETKNF2"
+};
+
+export const IMAGEBB_API_KEY = "1c63770e2fc72785f2d6f1560efe9bf5";
